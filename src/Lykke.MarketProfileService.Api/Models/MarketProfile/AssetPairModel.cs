fe23de3716511431b9758f5f1d72d54f6a86ee84
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-06-09 14:30:02 +0300

    Fix metadata for autorest

--- src/Lykke.MarketProfileService.Api/Models/MarketProfile/AssetPairModel.cs
+++ src/Lykke.MarketProfileService.Api/Models/MarketProfile/AssetPairModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lykke.MarketProfileService.Api.Models.MarketProfile
 {
     public class AssetPairModel
     {
+        [Required]
         public string AssetPair { get; set; }
+        [Required]
         public double BidPrice { get; set; }
+        [Required]
         public double AskPrice { get; set; }
+        [Required]
         public DateTime BidPriceTimestamp { get; set; }
+        [Required]
         public DateTime AskPriceTimestamp { get; set; }
     }
 }
